de13d15e41730a63af56ca498dc7f5a03571ca56
Kelly Eames <46878406+kellyeames@users.noreply.github.com>
2019-06-13 13:07:39 -0700

    Add files via upload

---
+++ foodkiosk/kiosk/item.py
@@ -0,0 +1,19 @@
+class Item():
+    def __init__(self, size, itemprice, itenmane)
+        self.size=size
+        self.itemprice=itemprice
+        self.itemname=itemname
+
+    def getSize(self):
+        return self.size
+    
+    def getItemPrice(self):
+        return self.itemprice
+    
+    def __str__(self):
+        return self.itemname
+
+    def getItemNumber(self):
+        return self.itemnumber
+
+  
