21cafcde0bd9d37f5aa2bc1b758de14e431397f7
Kelly Eames <46878406+kellyeames@users.noreply.github.com>
2019-06-13 11:01:52 -0700

    Add files via upload

---
+++ foodkiosk/kiosk/order.py
@@ -0,0 +1,12 @@
+class Order():
+    def __init__(self, detail):
+        self.detail=OrderDetail
+
+    def getOrderDetail(self):
+        return self.detail
+    
+    def getSelection(self):
+        return self.getSelection
+
+    def getItemName(self):
+        return self.itemname
